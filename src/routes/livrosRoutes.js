288c6dc0d4a0612aa57b0bb5ad92432bb90a0c1c
babinns <havenamehum@gmail.com>
2023-01-06 12:47:39 -0300

    API update

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -5,6 +5,10 @@ const router = express.Router();
 
 router
     .get("/livros", livroController.listarLivros)
+    .get("/livros/:id", livroController.listarLivroPorId)
+    .post("/livros", livroController.cadastrarLivro)
+    .put("/livros/:id", livroController.atualizarLivro)
+    
 
 
 export default router;
