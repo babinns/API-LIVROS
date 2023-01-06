1382d92081e9920b34a2396552a0e9d4d25f13d8
babinns <havenamehum@gmail.com>
2023-01-06 12:14:15 -0300

    commiting changes

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import livros from './livrosRoutes.js'
+
+
+const router = (app) => {
+    app.route('/').get((req,res) => {
+        res.status(200).send('Curso de node')
+    })
+
+    app.use(
+        express.json(),
+        livros
+    )
+}
+
+export default router
